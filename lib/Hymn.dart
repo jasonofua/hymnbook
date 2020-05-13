e0ba4439760b3dac3e1d6185dd7cfe2fdb612bc4
jasonofua <jasonofua@gmail.com>
2020-05-13 08:00:30 +0100

    chages

--- lib/Hymn.dart
+++ lib/Hymn.dart
@@ -1,23 +1,16 @@
-class Hymn{
-
-  String hymnNumber,hymnTitle;
+class Hymn {
+  String hymnNumber, hymnTitle;
   List<String> hymnContent;
 
   Hymn(this.hymnNumber, this.hymnTitle, this.hymnContent);
-
-
 }
 
-
 final List<Hymn> hymns = [
-
   Hymn("NO 2", "ALMIGHTY, MOST HOLY", [
     "ALMIGHTY, MOST HOLY,\nIs Thy name O LORD of Host!\nTo Thee we come with thanks\nAnd Thy holy name to praise,\nWe thank Thee LORD, for using us\nThy name to proclaim abroad.",
     "All wisdom, all pow’rs\nUnto Thee MOST HIGH belong\nWhen we come to praise Thee,\nWhat a blessing we have found;\nAnd the faithful ones by Thy grace\nShall reap the good of the land.",
     "Thou art justice and love!\nAnd all good things come from Thee,\nBy love, mercy and truth,\nThou hast brought us to Thy light\nBut when we had not come to Thee.\nWe were then without Thy light.",
-
   ]),
-
   Hymn("NO 2", "JEHOVAH, THE UNCREA-TED-", [
     "JEHOVAH, THE UNCREA-TED-\nCre-a-tor of good things.\nFrom age to age, Thou art the same-\nO Thou “ANCIENT OF DAYS”\nThou made man out of the dust,\nAnd gave him breath of life;\nThou made the earth for him, LORD\nTo dwell for evermore.",
     "Refrain:\nAll good things of the earth,\nTo the LORD belongeth;\nHe gives them to His faithful ones,\nWho keep His word of truth.",
@@ -25,20 +18,10 @@ final List<Hymn> hymns = [
     "All o’er Thy glorious body,\nShines brighter than the sun;\nThou sittest on Thy heav’nly throne,\nWith bright colours around,\nAs amber and sa’phire stone,\nThy heav’nly throne doth shine:\nMore glorious than a-ny throne,\nThat mortal man can make.",
     "When Thou thought of cre-a-ting,\nThou crea-ted “Logos” first\nBegotten One as Thy delight;\nBy whom good things were made.\nCame to earth as Christ Jesus,\nTo bring us back to Thee;\nAs in Adam all would die,\nE’en in Christ all shall live.",
   ]),
-
   Hymn("NO 1", "HOLY, HOLY, HOLY! LORD GOD ALMIGHTY", [
     "HOLY, HOLY, HOLY! LORD GOD ALMIGHTY!\nEarly in the morning our song shall rise to Thee:\nHOLY, HOLY, HOLY! Merciful and mighty!\nGOD in the highest, blessed Majesty.",
     "HOLY, HOLY, HOLY! All the saints adore Thee,\nCasting down their golden crowns around the glassy sea;\nCherubim and seraphim falling down before Thee,\nWhich wast, and art, and evermore shall be.",
     "HOLY, HOLY, HOLY! Though the darkness hide Thee,\nThough the eye of sinful man Thy glory may not see,\nOnly Thou art holy, there is none beside Thee\nPerfect in pow’r, in love and purity.",
     "HOLY, HOLY, HOLY! LORD GOD ALMIGHTY!\nAll Thy works shall praise Thy name, in earth and sky and sea,\nHOLY, HOLY, HOLY! Merciful and mighty!\nYea, let Thy Son be blessed eternally.",
   ]),
-
-
-
-
-
-
-
-
-
 ];
