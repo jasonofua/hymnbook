6c7ad16764e351eb1b95c77fdaa3dccde8e9192e
alphadude <alphadude4all@gmail.com>
2020-05-27 16:22:43 +0100

    minor fixes to the complete hymn

--- lib/Hymn.dart
+++ lib/Hymn.dart
@@ -27,11 +27,11 @@ class Hymn {
 
 final List<Hymn> hymns = [
 
-  Hymn("NO 2", "2 ALMIGHTY, MOST HOLY", [
-    "ALMIGHTY, MOST HOLY,\nIs Thy name O LORD of Host!\nTo Thee we come with thanks\nAnd Thy holy name to praise,\nWe thank Thee LORD, for using us\nThy name to proclaim abroad.",
-    "All wisdom, all pow’rs\nUnto Thee MOST HIGH belong\nWhen we come to praise Thee,\nWhat a blessing we have found;\nAnd the faithful ones by Thy grace\nShall reap the good of the land.",
-    "Thou art justice and love!\nAnd all good things come from Thee,\nBy love, mercy and truth,\nThou hast brought us to Thy light\nBut when we had not come to Thee.\nWe were then without Thy light.",
-
+  Hymn("NO 1", "1 HOLY, HOLY, HOLY! LORD GOD ALMIGHTY", [
+    "HOLY, HOLY, HOLY! LORD GOD ALMIGHTY!\nEarly in the morning our song shall rise to Thee:\nHOLY, HOLY, HOLY! Merciful and mighty!\nGOD in the highest, blessed Majesty.",
+    "HOLY, HOLY, HOLY! All the saints adore Thee,\nCasting down their golden crowns around the glassy sea;\nCherubim and seraphim falling down before Thee,\nWhich wast, and art, and evermore shall be.",
+    "HOLY, HOLY, HOLY! Though the darkness hide Thee,\nThough the eye of sinful man Thy glory may not see,\nOnly Thou art holy, there is none beside Thee\nPerfect in pow’r, in love and purity.",
+    "HOLY, HOLY, HOLY! LORD GOD ALMIGHTY!\nAll Thy works shall praise Thy name, in earth and sky and sea,\nHOLY, HOLY, HOLY! Merciful and mighty!\nYea, let Thy Son be blessed eternally.",
   ]),
 
   Hymn("NO 2", "2 JEHOVAH, THE UNCREA-TED-", [
@@ -42,12 +42,6 @@ final List<Hymn> hymns = [
     "When Thou thought of cre-a-ting,\nThou crea-ted “Logos” first\nBegotten One as Thy delight;\nBy whom good things were made.\nCame to earth as Christ Jesus,\nTo bring us back to Thee;\nAs in Adam all would die,\nE’en in Christ all shall live.",
   ]),
 
-  Hymn("NO 1", "1 HOLY, HOLY, HOLY! LORD GOD ALMIGHTY", [
-    "HOLY, HOLY, HOLY! LORD GOD ALMIGHTY!\nEarly in the morning our song shall rise to Thee:\nHOLY, HOLY, HOLY! Merciful and mighty!\nGOD in the highest, blessed Majesty.",
-    "HOLY, HOLY, HOLY! All the saints adore Thee,\nCasting down their golden crowns around the glassy sea;\nCherubim and seraphim falling down before Thee,\nWhich wast, and art, and evermore shall be.",
-    "HOLY, HOLY, HOLY! Though the darkness hide Thee,\nThough the eye of sinful man Thy glory may not see,\nOnly Thou art holy, there is none beside Thee\nPerfect in pow’r, in love and purity.",
-    "HOLY, HOLY, HOLY! LORD GOD ALMIGHTY!\nAll Thy works shall praise Thy name, in earth and sky and sea,\nHOLY, HOLY, HOLY! Merciful and mighty!\nYea, let Thy Son be blessed eternally.",
-  ]),
 
   Hymn("NO 3", "3 ALMIGHTY, MOST HOLY,", [
   "  1.	ALMIGHTY, MOST HOLY,\nIs Thy name O LORD of Host!\nTo Thee we come with thanks\nAnd Thy holy name to praise,\nWe thank Thee LORD, for using us\nThy name to proclaim abroad.",
